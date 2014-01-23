7ff1981e0b2a3f828013efe0738e7b721cc30ad5
vagrant <vagrant@precise32.(none)>
2014-01-23 18:49:25 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,51 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		//NOTE: must get rid of below for the toggle to work.
+		//$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
+
+		$("#submitBtn").click(updateProject);
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) {
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
+    //WE DELETED THIS:
+    //var containingProject = $(this).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+
+       //$("myelement").fadeIn();
+
+
+
+    }
+
+}
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
